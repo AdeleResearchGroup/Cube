2e03504d718178a5f250126487ec6d8eceb6d95f
Issac Noé <torito@gmail.com>
2012-08-07 15:41:26 +0200

    Modify Cilia Extention

--- src/extensions/cube-cilia-extension/src/main/java/fr/liglab/adele/cube/extensions/cilia/CiliaExtension.java
+++ src/extensions/cube-cilia-extension/src/main/java/fr/liglab/adele/cube/extensions/cilia/CiliaExtension.java
@@ -1,5 +1,12 @@
 package fr.liglab.adele.cube.extensions.cilia;
 
+import fr.liglab.adele.cilia.builder.Architecture;
+import fr.liglab.adele.cilia.builder.Builder;
+import fr.liglab.adele.cilia.exceptions.BuilderConfigurationException;
+import fr.liglab.adele.cilia.exceptions.BuilderException;
+import fr.liglab.adele.cilia.exceptions.CiliaException;
+import fr.liglab.adele.cilia.exceptions.CiliaIllegalParameterException;
+import fr.liglab.adele.cilia.exceptions.CiliaIllegalStateException;
 import fr.liglab.adele.cube.agent.AgentExtensionConfig;
 import fr.liglab.adele.cube.agent.CInstance;
 import fr.liglab.adele.cube.agent.CubeAgent;
@@ -12,50 +19,79 @@ import fr.liglab.adele.cube.util.id.CInstanceUID;
 
 public class CiliaExtension extends AbstractExtension {
 
+	private final String chainId;
+	private final CiliaExtensionFactory cfactory;
+
+
 
 	public CiliaExtension(CubeAgent agent, IExtensionFactory factory,
 			AgentExtensionConfig config) {
 		super(agent, factory, config);
-		
-		
+		chainId = agent.getArchetype().getId();
+		this.cfactory =(CiliaExtensionFactory) getExtensionFactory();  
 	}
 
 	@Override
 	public void start() {
 		System.out.println("\n\n\nCILIA EXTENSION started.\n\n");
-		
+
 		// 1. create cilia chain
-		
-		// 2. read the runtime model, and see if there exist already cube component instances created!
-		for (CInstance i : getCubeAgent().getRuntimeModel().getCInstances(CoreExtensionFactory.ID, Component.NAME, CInstance.VALID)) {
-			createMediator(i);
+		Builder ciliaBuilder = cfactory.getCiliaContext().getBuilder();
+		try {
+			Architecture chain = ciliaBuilder.create(chainId);
+			// 2. read the runtime model, and see if there exist already cube component instances created!
+			for (CInstance i : getCubeAgent().getRuntimeModel().getCInstances(CoreExtensionFactory.ID, Component.NAME, CInstance.VALID)) {
+				createMediator(i, chain);
+			}
+			ciliaBuilder.done();
+		} catch (CiliaException e) {
+			e.printStackTrace();
+		}
+		//3. Initialize the chain
+		try {
+			cfactory.getCiliaContext().getApplicationRuntime().startChain(chainId);
+		} catch (CiliaIllegalParameterException e) {
+			e.printStackTrace();
+		} catch (CiliaIllegalStateException e) {
+			e.printStackTrace();
 		}
 	}
-	
+
 	public void validatedInstance(CInstance coi) {
 		if (coi != null) {			
 			if (coi.getCType().getNamespace().equalsIgnoreCase(CoreExtensionFactory.ID) && coi.getCType().getName().equalsIgnoreCase(Component.NAME)) {
-				createMediator(coi);
+				Builder ciliaBuilder = cfactory.getCiliaContext().getBuilder();
+				Architecture chain;
+				try {
+					chain = ciliaBuilder.get(chainId);
+					createMediator(coi, chain);
+					ciliaBuilder.done();
+				} catch (CiliaException e) {
+					e.printStackTrace();
+				} 
 			}
 		}
 	}
-	
+
 	/**
 	 * Create Cilia Mediator and add it to the cilia local chain
 	 * @param i
+	 * @throws BuilderException 
+	 * @throws BuilderConfigurationException 
 	 */
-	private void createMediator(CInstance i) {		
+	private void createMediator(CInstance i, Architecture chain) throws BuilderConfigurationException, BuilderException {		
 		String componentType = i.getCType().getId();
-		// TODO create mediator of componentType type and add it to the chain
-		
+		// create mediator of componentType type and add it to the chain
+		chain.create().mediator().type(componentType).id(i.getLocalId());
+		//were are the properties??
 		for (CInstanceUID instanceUID : ((ComponentInstance)i).getOutComponents()) {
 			// TODO you can get the object instance of this UID from the runtime model
 			CInstance inst = getCubeAgent().getRuntimeModel().getCInstance(instanceUID);
 			// this is the next mediator where the actual one is connected.
 			// TODO check if this next mediators is already created, if so, create binding between the two
-						
+
 		}
-		
+
 	}
 
 	public void stop() {
@@ -70,11 +106,11 @@ public class CiliaExtension extends AbstractExtension {
 	public String toString() {
 		String out = "";
 		out += "+ CiliaExtension\n";
-		
+
 		// TODO print what you want :)
 		// when we call the "ext" command, this toString method is called to show 
 		// some information about this extension internal work.
-		
+
 		return out;
 	}
 
